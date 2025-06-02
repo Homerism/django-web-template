e16ef7a5f30a40843a5b38bcbe137b5467644367
Homerism <jahmelhomer@outlook.com>
2025-06-01 22:58:52 -0400

    Third Commit 1

--- web-template/urls.py
+++ web-template/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for web-template project.
+URL configuration for web-template project-1.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
