eb7fb59147154933158b824cfaa9b39f738344f4
Homerism <jahmelhomer@outlook.com>
2025-06-04 23:37:02 -0400

    Sixth Commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreatelView, PostUpdatelView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreatelView, PostUpdatelView, PostDeleteView, UserPostListView
 from .import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreatelView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdatelView.as_view(), name='post-update'),
--- web-template/settings.py
+++ web-template/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +139,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
